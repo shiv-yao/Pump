a4252b2dc8ab14779fff9d164efe7a27fa66b179
shiv-yao <153885823+shiv-yao@users.noreply.github.com>
2026-04-09 20:46:14 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,13 +1,6 @@
-from fastapi import FastAPI
-import asyncio
+import sys
+import os
+
+sys.path.append(os.getcwd())
+
 from app.engine.main import main_loop
-
-app = FastAPI()
-
-@app.on_event("startup")
-async def startup():
-    asyncio.create_task(main_loop())
-
-@app.get("/")
-def root():
-    return {"status": "running"}
