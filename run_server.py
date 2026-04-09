0a42753c06ae6e3ffe3ce47659018b9806422168
shiv-yao <153885823+shiv-yao@users.noreply.github.com>
2026-04-10 00:03:22 +0800

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,7 +1,25 @@
 import os
 import uvicorn
 
-port = int(os.environ.get("PORT", "8000"))
+def get_port():
+    try:
+        return int(os.environ.get("PORT", "8000"))
+    except Exception:
+        return 8000
+
+def main():
+    port = get_port()
+
+    print("🚀 RUN SERVER")
+    print("PORT =", port)
+
+    uvicorn.run(
+        "app.server:app",
+        host="0.0.0.0",
+        port=port,
+        log_level="info",
+        access_log=True,
+    )
 
 if __name__ == "__main__":
-    uvicorn.run("app.server:app", host="0.0.0.0", port=port)
+    main()
