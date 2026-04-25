270ade1ec1f2aa42b81d0652e18a197e614f2ebc
shiv-yao <153885823+shiv-yao@users.noreply.github.com>
2026-04-25 13:25:26 +0800

    Delete app directory

--- app/execution_gateway/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-TRADING_API_BASE = os.getenv("TRADING_API_BASE", "").rstrip("/")
-POLYMARKET_API_KEY = os.getenv("POLYMARKET_API_KEY", "").strip()
-
-GATEWAY_TIMEOUT = float(os.getenv("GATEWAY_TIMEOUT", "5"))
-GATEWAY_RETRY = int(os.getenv("GATEWAY_RETRY", "2"))
